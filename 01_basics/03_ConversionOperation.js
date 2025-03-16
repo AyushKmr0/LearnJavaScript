6d4df6c5a0db50d025a385e3742b40badedf7166
Ayush kumar <165952944+AyushKmr0@users.noreply.github.com>
2025-03-16 10:41:46 +0000

    Operations

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,8 +1,71 @@
 let score = "Ayush"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 
 let valueInNumber = Number(score)
-console.log(valueInNumber);     // NaN
+// console.log(typeof(valueInNumber));  //  type => number
+// console.log(valueInNumber);     // NaN
+
+
+
+/*
+"333" => 333
+"32abc" => NaN
+true => 1, false => 0
+*/
+
+
+let isLoggedIn = "Ayush"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+
+/*
+1 => true; 0 => false
+"" => false
+"Ayush" => true
+*/
+
+
+
+let someNum = 45
+
+let stringNum = String(someNum);
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+
+
+
+// ********************* Operations *********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**2)
+
+
+
+let str1 = "Hello"
+let str2 = "World"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+// console.log("1" + 2);       // 12
+// console.log(1 + "2");       // 12
+// console.log("1" + 2 + 2);   // 122
+// console.log(1 + 2 + "2");   // 32
